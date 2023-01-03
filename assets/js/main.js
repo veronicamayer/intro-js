10ac28b62e9604136672edd706c4f2a4104d2c67
veronicamayer <mayerveronica.vm@gmail.com>
2023-01-03 14:20:08 +0100

    first commit

---
+++ assets/js/main.js
@@ -0,0 +1,207 @@
+/*----------
+    Übung 1 Lev 1_2
+-----------*/
+
+// This is a single line JavaScript comment
+document.write("I have comments in my JavaScript code!");
+//document.write("You can't see this!");
+//-->
+
+document.write("I have multi-line comments!");
+/*document.write("You can't see this!");
+document.write("You can't see this!");
+document.write("You can't see this!");
+document.write("You can't see this!");
+document.write("You can't see this!");
+document.write("You can't see this!");
+document.write("You can't see this!");*/
+
+/*----------
+    Übung 1 Lev 1_7
+-----------*/
+
+let helloWorld = "Hello World";
+console.log(helloWorld);
+
+const pi = 3.14;
+console.log(pi);
+
+const burjDubai = "828m";
+console.log(burjDubai);
+
+let fullname = "Jan Schmidt";
+console.log(fullname);
+
+const eifelTower = "324m";
+console.log(eifelTower);
+
+let camelCase = "camelCase";
+console.log(camelCase);
+
+let kursStatus = true;
+console.log(kursStatus);
+
+/*----------
+    Übung 2 Lev 1_1
+-----------*/
+
+a = 12;
+b = a * 3;
+console.log(b);
+document.write(b);
+
+console.log(document.URL);
+
+console.log("Hello World");
+console.log("Anton");
+
+let firstName = "Anton";
+console.log(firstName);
+
+let age = 28;
+console.log(age);
+
+let job = "Trainer";
+console.log(job);
+
+let married = true;
+console.log(married);
+
+console.log(firstName + " ist " + age + " Jahre alt, von Beruf " + job + " und ist verheiratet: " + married);
+
+/*----------
+    Übung 3 Lev 1_5
+-----------*/
+
+let carName = "BMW";
+console.log(carName);
+
+let x = 150;
+console.log(x);
+
+let y = 50;
+console.log(y);
+
+let z = x + y;
+console.log(z);
+
+let newFirstName; let newLastName; let newAge;
+
+firstName = "John"; lastName = "Doe"; age = 35;
+
+console.log(firstName); console.log(lastName); console.log(age);
+
+/*----------
+    Übung 3 Lev 1_6
+-----------*/
+
+let newX = 34;
+newX = 67;
+console.log("newX: " + newX);
+
+/* const newY = 34
+newY = 67
+console.log("newY: " + newY);
+ */
+
+/*----------
+    Übung 4 Lev 1_12
+-----------*/
+
+let score = 5 + 5 * 10;
+console.log("Ergebnis: " + score);
+
+score = (5+5)*10;
+console.log("Ergebnis: " + score);
+
+score = score + 10;
+console.log(score);
+
+score += 10;
+console.log(score);
+
+let zahl = 1;
+zahl = zahl +1;
+zahl += 1;
+console.log(zahl);
+
+zahl++;
+console.log(zahl);
+
+zahl--;
+console.log(zahl);
+
+/*----------
+    Übung 4 Lev 1_13
+-----------*/
+
+// Addition
+let addition_operator = 1 + 1;
+console.log("addition: " + addition_operator);
+
+// Subtraktion
+let subtraktion_operator = 2 - 1;
+console.log("subtraktion: " + subtraktion_operator);
+
+// Multiplikation
+let multiplication_operator = 2 * 2;
+console.log("multiplication: " + multiplication_operator);
+
+// Division
+let division_operator = 4 / 2;
+console.log("division: " + division_operator);
+
+// Modul: zeigt an, was der Rest sein würde.
+let modulus_operator = 14 % 5;
+console.log("modulus: " + modulus_operator);
+
+/*----------
+    Übung 5 Lev 1_11
+-----------*/
+
+let anotherX = "Hello";
+let anotherY = "World<br>";
+
+let anotherZ = anotherX + anotherY;
+document.write(anotherZ);
+
+let n = anotherX + " " + anotherY;
+document.write(n);
+
+anotherX += "World";
+document.write(anotherX);
+
+let meinString = "Ich bin Erster";
+meinString += " Ich komme auf Platz zwei";
+console.log(meinString);
+
+/*----------
+    Übung 6 Lev 1_1
+-----------*/
+
+document.write("<h1>Hello World!</h1><p>Have a nice day!</p>");
+let myText = "Hello again";
+document.write(myText);
+let vorname; let nachname;
+vorname = "Veronica"; nachname = "Mayer";
+document.write("<p>Name: " + vorname + "</p><p>Nachname: " + nachname + "</p>")
+
+/*----------
+    Übung 7 Lev 1_4
+-----------*/
+
+/* window.alert("Hallo Welt"); */
+/* window.prompt("Bitte geben Sie Ihren Namen ein."); */
+/* window.prompt("Bitte geben Sie Ihren Namen ein", "Cancel"); */
+/* window.confirm("Stimmen Sie meiner Meinung zu?"); */
+
+/* let alter = prompt( "Gib mir bitte dein Alter:" );
+console.log(alter); */
+
+/* let anotherB = 5;
+let anotherA = anotherB * 5 -3;
+alert(anotherA); */
+
+/* let mann = "Robert Wadlow ist der größte Mann der Welt : ";
+let grosse = "2,72m";
+alert(mann + grosse); */
